bd2a46387262958889b012b0e5ad600d0c372906
jendek.tomas@gmail.com <jendek.tomas@gmail.com>
2013-11-16 08:04:12 -0800

    mailer

--- app/mailers/position_mailer.rb
+++ app/mailers/position_mailer.rb
@@ -1,3 +1,8 @@
 class PositionMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def welcome_email(email, link)
+    @link = link
+    mail(to: email, subject: 'Welcome to My Awesome Site')
+  end
 end
